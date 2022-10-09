04d26ec2c43ed3decf592c3beabb6f6f2caa50f7
tranmausa <currykobejames@gmail.com>
2022-10-10 00:26:33 +0700

    update_API_kitchen_shipper

--- src/auth/dto/register-kitchen.dto.ts
+++ src/auth/dto/register-kitchen.dto.ts
@@ -8,19 +8,19 @@ export class RegisterKitchenDTO {
   password: string;
   @ApiProperty()
   fullName: string;
-  @ApiProperty({
-    type: Date,
-    description: 'dateOfBirth',
-    default: new Date().toISOString().slice(0, 10),
-  })
-  public DOB: Date;
+  // @ApiProperty({
+  //   type: Date,
+  //   description: 'dateOfBirth',
+  //   default: new Date().toISOString().slice(0, 10),
+  // })
+  // public DOB: Date;
 
   @ApiProperty()
   @IsEmail()
   email: string;
 
-  @ApiProperty()
-  avatar: string;
+  // @ApiProperty()
+  // avatar: string;
 
   @ApiProperty()
   address: string;
